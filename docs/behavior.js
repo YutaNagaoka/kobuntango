92c13197ee546095735bac7e7256ddf78f357885
Yuta <shirasu.okatin@gmail.com>
2018-06-19 23:48:47 +0900

    [add] show words randomly.
    be able to show words randomly from hash when button is pushed.

--- docs/behavior.js
+++ docs/behavior.js
@@ -1,5 +1,22 @@
-var button = document.getElementById("nextButton");
+const wordDict = {
+    "あいぎやう": "かわいらしさ 思いやり",
+    "あいなし": "おもしろくない 気に食わない ただもう",
+    "あからさまなり": "ちょっと ほんのしばらく 全く",
+    "あきらむ": "明らかにする 晴らす"
+}
+
+const button = document.getElementById("nextButton");
 button.onclick = function () {
-    const elem = document.getElementById("panel-in").value;
-    elem.textContent = "HOGE";
+    const elem = document.getElementById("panel-in");
+    const content = selectPairRandomly(wordDict);
+    elem.textContent = content[0];
 };
+
+
+function selectPairRandomly(wordDict) {
+    const keysArray = Object.keys(wordDict);
+    const dictLength = keysArray.length;
+    const index = Math.floor(Math.random() * dictLength);
+    const key = keysArray[index];
+    return [key, wordDict[key]];
+}
