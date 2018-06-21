1d5f92b58d64a4b761a338a3a38474775739bfbb
Yuta <shirasu.okatin@gmail.com>
2018-06-21 21:10:52 +0900

    [add] improve displayment.
    insert new line in answer when it has multiple meaning.

--- docs/behavior.js
+++ docs/behavior.js
@@ -1,3 +1,8 @@
+/**
+ * Select key randomly from hash.
+ * @param {Object} wordDict : Input hash.
+ * @returns {String} key : Key selected randomly.
+ */
 const selectKeyRandomly = (wordDict) => {
     const keysArray = Object.keys(wordDict);
     const dictLength = keysArray.length;
@@ -6,8 +11,28 @@ const selectKeyRandomly = (wordDict) => {
     return key;
 };
 
-// const wordDict = getDict("a");
-const wordDict = words_1;
+/**
+ * Get value from wordDict with key and replace space for <br> tag.
+ * @param {String} key : Key to indicate value.
+ * @returns {String} formattedAnswer
+ */
+const formatAnswer = (key) => {
+    const answerText = wordDict[key];
+    const splittedAnswer = answerText.split(" ");
+    let formattedAnswer = splittedAnswer.map(a => a + "<br>").join(" ");
+    return formattedAnswer;
+};
+
+/**
+ * 
+ * @param {String} key : Key to indicate value you want to delete.
+ * @returns {Void}
+ */
+const archiveWord = (key) => {
+    delete wordDict[key];
+};
+
+const wordDict = words_2;
 const buttonElems = document.getElementsByClassName("phaseButton");
 const buttons = Array.from(buttonElems);
 const kogo = document.getElementById("kogo");
@@ -15,12 +40,20 @@ const gendaigo = document.getElementById("gendaigo");
 
 // "次へ"のボタン
 buttons[1].onclick = function () {
-    const selectedWord = selectKeyRandomly(wordDict);
-    kogo.textContent = selectedWord;
+    const question = selectKeyRandomly(wordDict);
+    if (question === undefined) {
+        kogo.textContent = "終了";
+    }
+    else {
+        kogo.textContent = question;
+    }
     gendaigo.textContent = "";
 };
 
 // "意味を表示"のボタン
 buttons[0].onclick = function () {
-    gendaigo.textContent = wordDict[kogo.textContent];
+    const question = kogo.textContent;
+    const answer = formatAnswer(question);
+    gendaigo.innerHTML = answer;
+    archiveWord(question);
 };
